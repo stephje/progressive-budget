73466311dce8d2836d4cdecd327a8fc70a515b22
stephje <s.jenkins3018@gmail.com>
2021-10-01 12:39:07 +0800

    deleted file that was not needed

--- todo.js
+++
@@ -1,9 +0,0 @@
-// In the main app javascript: 
-// generate the content based on template
-// register service worker
-
-//  The service worker: 
-// import data from js file
-// create a list of all the files to be cached, both from the app shell and the content
-// install the service worker
-// fetch content from the cache if it is available there
