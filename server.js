ff137821f372c7eecc18ae7634f667e4f42b1ae2
Stephanie Jenkins <s.jenkins3018@gmail.com>
2021-10-30 16:58:45 +0800

    Added connection to MongoDB Atlas rather than local db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
