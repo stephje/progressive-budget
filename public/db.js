d1c016e7e3cd43e4742543ec1b7eafff18501f83
Stephanie Jenkins <s.jenkins3018@gmail.com>
2021-10-30 16:30:18 +0800

    Added updateDatabase function to post to mongoDB

--- public/db.js
+++ public/db.js
@@ -9,6 +9,41 @@ request.onerror = function (event) {
   console.log(`Something went wrong. ${event.target.errorCode}`);
 };
 
+function updateDatabase() {
+  //Open a transaction and access data store
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
+
+  //Get all records
+  store.getAll().onsuccess = function (event) {
+    const allRecords = event.target.result;
+    if (allRecords.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(allRecords),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (data.length > 0) {
+            //Open transaction and clear store
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+            const currentStore = transaction.objectStore("BudgetStore");
+            currentStore.clear();
+          }
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
+  };
+}
+
 //Success handler
 request.onsuccess = function (event) {
   console.log(`Success!`);
@@ -37,10 +72,10 @@ request.onupgradeneeded = function (event) {
 
 //Create a transaction, access the object store and add a record
 const saveRecord = (record) => {
-    const transaction = db.transaction(['BudgetStore'], 'readwrite');
-    const store = transaction.objectStore('BudgetStore');
-    store.add(record);
-  };
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
+  store.add(record);
+};
 
-// Listen for whether app is online, and if it is, update the database 
+// Listen for whether app is online, and if it is, update the database
 window.addEventListener("online", updateDatabase);
